f2837cece98e3652fada3d1fa48fb339d98fb5d4
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-07-14 22:27:44 +0530

    add array fot props value

--- basic-react-project/src/ProductTab.jsx
+++ basic-react-project/src/ProductTab.jsx
@@ -5,7 +5,7 @@ function ProductTab(){
    let option2 ={a:"hi-tect" ,b:"durable", c:"fast"};
    return(
     <>
-    <Product title="phone" price={30000} feature={option} feature2={option2.a}/>
+    <Product title="phone" price={30000} feature={["hi-tech","durable","fast"]} feature2={option2.a}/>
     <Product title="laptop" price={32000}/>
     <Product title="pen" price={23000}/>
     </>
