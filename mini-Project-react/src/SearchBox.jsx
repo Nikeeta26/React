b08c1df9ffb3b90eec248f4a5f2ba2dd97c958d2
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-08-02 20:15:17 +0530

    create css file and add css

--- mini-Project-react/src/SearchBox.jsx
+++ mini-Project-react/src/SearchBox.jsx
@@ -1,15 +1,16 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import "./SearchBox.css";
 export default function SearchBox(){
     return(
-        <>
+        <div className='SearchBox'>
         <h2>Search for the weather</h2>
         <TextField id="city" label="City Name" variant="outlined" required />
         <br></br><br></br>
         <Button variant="contained" type="submit" >
         Search
       </Button>
-        </>
+        </div>
         
     )
 }
